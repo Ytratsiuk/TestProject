366d4a05a0447f7e1f7092ce5aebf4787f54848f
Ytratsiuk <ytratsiuk@gmail.com>
2023-01-11 20:02:29 -0700

    TestProgect commit3

--- src/homework/LoopNumbers.java
+++ src/homework/LoopNumbers.java
@@ -12,18 +12,40 @@ public class LoopNumbers {
                 System.out.println(b);
             }
         }
-        for (int c = 5; c <=50; c++) {
+        for (int c = 5; c <= 50; c++) {
             if (c % 5 == 0) {
                 System.out.println(c);
             }
         }
-       for(int d=1;d<=100;d++) {
-           System.out.print(d + " ");
+        for (int d = 1; d <= 100; d++) {
+            System.out.print(d + " ");
 
-       }
-        for(int e=100;e>=1;e--) {
-            System.out.println(e+" ");
+        }
+        for (int e = 100; e >= 1; e--) {
+            System.out.println(e + " ");
         }
 
+//        Print even numbers from 20 to 1 (2 ways)
+        for (int f = 20; f >= 1; f--) {
+            System.out.println(f);
+        }
+        for (int g = 1; g <= 20; g++) {
+            System.out.println(g);
+        }
+//        Print odd numbers between 20 and 50 (2 ways)
+
+        for (int i = 20; i <= 50; i++) {
+            if (i % 2 == 0) {
+
+            } else {
+                System.out.println(i);
+            }
+        }
+        for (int i = 20; i <= 50; i++) {
+            if (i % 2 != 0) {
+            System.out.println(i);
+            }
+        }
     }
 }
+
