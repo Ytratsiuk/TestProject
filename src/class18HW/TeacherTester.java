3235b91ca6cfcd328a9e91be702de4f4b2523017
Ytratsiuk <ytratsiuk@gmail.com>
2023-02-05 21:08:41 -0700

    add homework

---
+++ src/class18HW/TeacherTester.java
@@ -0,0 +1,13 @@
+package class18HW;
+
+public class TeacherTester {
+    public static void main(String[] args) {
+        Teacher myTeacher = new Teacher();
+        PianoTeacher pianoTeacher = new PianoTeacher();
+        MathTeacher mathTeacher = new MathTeacher();
+        System.out.println(mathTeacher.infoExperience(6));
+        System.out.println(mathTeacher.task(5,8));
+        System.out.println(pianoTeacher.bigSmallClass(5));
+        System.out.println(myTeacher.idCreator("Yuliya","Trarsiuk",5));
+    }
+}
---
+++ src/class18HW/Two.java
@@ -0,0 +1,6 @@
+/*package class18HW;
+
+public class Two
+//Write program to inherit class A that has method printF which is static and
+// call or reuse that method into class B*/
+
