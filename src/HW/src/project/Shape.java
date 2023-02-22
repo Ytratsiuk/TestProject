ac1fbfd6b9538c220278210ad235fe5d70ff35e4
Ytratsiuk <ytratsiuk@gmail.com>
2023-02-21 22:56:25 -0700

    project

--- src/HW/src/project/Shape.java
+++ src/HW/src/project/Shape.java
@@ -1,6 +1,63 @@
 package HW.src.project;
+
 //Create an Interface 'Shape' with undefined methods as calculateArea and calculatePerimiter.
 // Create 2 classes Circle & Square that implements functionality defined in the Shape Interface. Test your code.
+class TestingTheCode{
+    public static void main(String[] args) {
+        Circle circle = new Circle(4.9);
+        Square square = new Square(5.3);
+        System.out.println(circle.calculateArea());
+        System.out.println(circle.calculatePerimiter());
+        System.out.println(square.calculateArea());
+        System.out.println(square.calculatePerimiter());
+    }
+}
 interface Shape {
 
+    double calculateArea();
+
+    double calculatePerimiter();
+
+}
+
+class Circle implements Shape {
+    double r;
+
+    Circle(double r) {
+        this.r = r;
+    }
+
+    final double pi = 3.14159;
+
+    //A=πr2
+    @Override
+    public double calculateArea() {
+        return pi * r * r;
+    }
+
+    //C=2πr
+    @Override
+    public double calculatePerimiter() {
+        return 2 * pi * r;
+    }
+}
+
+class Square implements Shape {
+    //A=a2
+    double a;
+
+    Square(double a) {
+        this.a = a;
+    }
+
+    @Override
+    public double calculateArea() {
+        return 2 * a;
+    }
+
+    //P=4a
+    @Override
+    public double calculatePerimiter() {
+        return 4 * a;
+    }
 }
--- src/HW/src/project/WebDriver.java
+++ src/HW/src/project/WebDriver.java
@@ -2,5 +2,117 @@ package HW.src.project;
 
 //Provide Implementation for the diagram below. Then create a test class in which you need to create Objects of
 //ChromeDriver, FirefoxDrive and SafariDriver classes and see which methods available to them.
-public class Diagram {
+class TestingClass {
+    public static void main(String[] args) {
+        RemoteWebDriver[] webDrevers = {new ChromeDriver(), new SafariDriver(), new FirefoxDriver()};
+        for (RemoteWebDriver drivers : webDrevers) {
+            System.out.println(drivers.getTitle());
+            drivers.open();
+            drivers.close();
+            drivers.navigate();
+
+        }
+    }
+
+}
+
+interface WebDriver {
+    void open();
+
+    void close();
+
+    String getTitle();
+}
+
+interface TakesScreenShot {
+    void takeScreenShot();
+}
+
+interface RemoteWebDriver extends WebDriver, TakesScreenShot {
+    void navigate();
+}
+
+class ChromeDriver implements RemoteWebDriver {
+
+    @Override
+    public void open() {
+        System.out.println("Chrome opens");
+    }
+
+    @Override
+    public void close() {
+        System.out.println("Chrome closes");
+    }
+
+    @Override
+    public String getTitle() {
+        return "Chrome log in sign up";
+    }
+
+    @Override
+    public void takeScreenShot() {
+        System.out.println("Chrome takes screenshots");
+    }
+
+    @Override
+    public void navigate() {
+        System.out.println("www.chrome.com");
+    }
+}
+
+class FirefoxDriver implements RemoteWebDriver {
+
+    @Override
+    public void open() {
+        System.out.println("Firefox opens");
+
+    }
+
+    @Override
+    public void close() {
+        System.out.println("Firefox closes");
+    }
+
+    @Override
+    public String getTitle() {
+        return "Firefox log in";
+    }
+
+    @Override
+    public void takeScreenShot() {
+        System.out.println("Firefox takes a screenshot");
+    }
+
+    @Override
+    public void navigate() {
+        System.out.println("www.firefox.com");
+    }
+}
+
+class SafariDriver implements RemoteWebDriver {
+
+    @Override
+    public void open() {
+        System.out.println("Safari opens");
+    }
+
+    @Override
+    public void close() {
+        System.out.println("Safari closes");
+    }
+
+    @Override
+    public String getTitle() {
+        return "Safari log in";
+    }
+
+    @Override
+    public void takeScreenShot() {
+        System.out.println("Safari takes a screenshot");
+    }
+
+    @Override
+    public void navigate() {
+        System.out.println("www.safari.com");
+    }
 }
