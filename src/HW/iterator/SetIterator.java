574802a433492ac03d003d725ae68914505e2936
Ytratsiuk <ytratsiuk@gmail.com>
2023-02-27 22:10:44 -0700

    iterator Set

---
+++ src/HW/iterator/SetIterator.java
@@ -0,0 +1,27 @@
+package HW.iterator;
+
+import java.util.Iterator;
+import java.util.LinkedHashSet;
+import java.util.Set;
+//Create a Set collection in which you want to preserve an order of inserted String Objects + 1 null value.
+public class SetIterator {
+    public static void main(String[] args) {
+        Set<String> words = new LinkedHashSet<>();
+        words.add("null");
+        words.add("Sohil");
+        words.add("Diego");
+        words.add("Alijon");
+        words.add("Asel");
+        words.add("Sumair");
+
+        Iterator<String> iterator = words.iterator();
+        while (iterator.hasNext()) {
+            String name = iterator.next();
+            System.out.println(name);
+        }
+        for ( String word : words){
+            System.out.println(word);
+        }
+    }
+}
+
