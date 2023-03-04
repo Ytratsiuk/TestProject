ca7e28d562a7ebc168ed1f1de4df406b2fc0611a
Ytratsiuk <ytratsiuk@gmail.com>
2023-03-04 14:29:42 -0700

    Sort Map by Value

---
+++ src/class30/map.java
@@ -0,0 +1,18 @@
+/*package class30;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class map {
+    public static void main (String []args){
+        HashMap<String, Object> address = new HashMap <>();
+        address.put("Street", "Patrick ST");
+        address.put("Suite",265);
+        address.put("City","Vienna");
+        address.put("Zip", 22180);
+        address.put("Country","United State");
+        Map.Entry<String, Object> keysValues = address.entrySet();
+        System.out.println (keysValues);
+    }
+
+}*/
--- src/class31/Exel.java
+++ src/class31/Exel.java
@@ -1,5 +1,25 @@
 package class31;
 
+import class21.Programming;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
 public class Exel {
+    static public void main(String[] args) throws IOException {
+        String path = "Files/Book1.xlsx";
+        FileInputStream fileInputStream = new FileInputStream(path);
+        XSSFWorkbook xssfWorkbook =new XSSFWorkbook(fileInputStream);
+        XSSFSheet sheet1 = xssfWorkbook.getSheet("Sheet1");
+        Row row = sheet1.getRow(1);
+        Cell cell = row.getCell(1);
+        System.out.println(cell);
+
+    }
 
 }
